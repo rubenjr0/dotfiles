4c90b176e3008885dad956f37f47ba5aba6a9ea1
rubenjr0 <rubenjr0@proton.me>
2025-11-24 23:38:37 +0100

    nobara-helium

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    alias ls="eza --group-directories-first"
+    alias lt="eza -T --group-directories-first"
+    alias ff="fastfetch"
+
+    fish_config theme choose flexoki-dark
+
+    zoxide init fish | source
+    starship init fish | source
+    direnv hook fish | source
+end
+
+set -gx WASMTIME_HOME "$HOME/.wasmtime"
+
+string match -r ".wasmtime" "$PATH" >/dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
